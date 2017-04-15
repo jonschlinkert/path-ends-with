fe86f81b8978757a7a39d5d9a7c20d3c869091fe
jonschlinkert <jon.schlinkert@sellside.com>
2017-04-14 21:07:24 -0400

    completely refactor

--- index.js
+++ index.js
@@ -1,20 +1,60 @@
 'use strict';
 
-/**
- * Module dependencies
- */
-
 var endsWith = require('ends-with');
-var normalize = require('normalize-path');
+var normalizePath = require('normalize-path');
 
-/**
- * Return true if `filepath` ends with the given `string`
- *
- * @param  {String} `filepath`
- * @param  {String} `string`
- * @return {Boolean}
- */
+module.exports = function(filepath, substr, allowPartialMatch) {
+  if (typeof filepath !== 'string') {
+    throw new TypeError('expected filepath to be a string');
+  }
+  if (typeof substr !== 'string') {
+    throw new TypeError('expected substring to be a string');
+  }
 
-module.exports = function(fp, str) {
-  return endsWith(normalize(fp, false), normalize(str, false));
+  if (substr === '') {
+    return false;
+  }
+
+  // return true if the given strings are an exact match
+  if (filepath === substr) {
+    return true;
+  }
+
+  filepath = normalize(filepath, false);
+  substr = normalize(substr, false);
+
+  // return true if normalized strings are an exact match
+  if (filepath === substr) {
+    return true;
+  }
+
+  if (endsWith(filepath, substr)) {
+    if (allowPartialMatch === true) {
+      return true;
+    }
+
+    // if the first character in the substring is a
+    // dot or slash, we can consider this a match
+    var ch = substr.charAt(0);
+    if (ch === '.' || ch === '/') {
+      return true;
+    }
+
+    // since partial matches were not enabled, we can
+    // only consider this a match if the next character
+    // is a dot or a slash
+    var slen = substr.length;
+    var next = filepath.slice(-(slen + 1), -slen);
+    return next === '.' || next === '/';
+  }
+
+  return false;
 };
+
+function normalize(str) {
+  str = normalizePath(str, false);
+  if (str.slice(0, 2) === './') {
+    str = str.slice(2);
+  }
+  return str;
+}
--- test.js
+++ test.js
@@ -1,29 +1,51 @@
+'use strict';
+
 /*!
  * path-ends-with <https://github.com/jonschlinkert/path-ends-with>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT License
+ * Copyright (c) 2014, 2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 require('mocha');
-require('should');
+var assert = require('assert');
 var endsWith = require('./');
 
-
 describe('endsWith', function() {
-  it('should return true if the path ends with the given string', function() {
-    endsWith('foo\\bar\\baz\\', '/').should.equal(true);
-    endsWith('foo\\bar\\baz\\', '\\').should.equal(true);
-    endsWith('foo/bar/baz/', '/').should.equal(true);
-    endsWith('foo/bar/baz', 'baz').should.equal(true);
-    endsWith('foo/bar/baz', 'bar/baz').should.equal(true);
-    endsWith('foo\\bar\\baz.md', 'baz.md').should.equal(true);
-    endsWith('foo\\bar\\baz.md', 'bar/baz.md').should.equal(true);
-    endsWith('foo\\bar\\baz.md', '.md').should.equal(true);
+  it('should be false when the given substring is an empty string', function() {
+    assert(!endsWith('foo\\bar\\baz\\', ''));
   });
 
-  it('should return false if the path does not end with the given string', function() {
-    endsWith('foo\\bar\\baz.md', '/').should.equal(false);
-    endsWith('foo\\bar\\baz.md', 'baz').should.equal(false);
+  it('should work when the path ends with unix slashes', function() {
+    assert(endsWith('foo/bar/baz/', '/'));
+    assert(endsWith('foo/bar/baz//', '//'));
+    assert(!endsWith('foo/bar/baz', '/'));
+  });
+
+  it('should work when the path ends with windows slashes', function() {
+    assert(endsWith('foo\\bar\\baz\\', '/'));
+    assert(endsWith('foo\\bar\\baz\\', '\\'));
+    assert(endsWith('foo\\bar\\baz\\\\', '\\\\'));
+  });
+
+  it('should be true when the path ends with a string', function() {
+    assert(endsWith('foo/bar/baz', 'baz'));
+    assert(endsWith('foo/bar/baz', '/baz'));
+    assert(endsWith('foo/bar/baz', 'bar/baz'));
+    assert(endsWith('foo\\bar\\baz.md', 'baz.md'));
+    assert(endsWith('foo\\bar\\baz.md', 'bar/baz.md'));
+    assert(endsWith('foo\\bar\\baz.md', '.md'));
+    assert(endsWith('foo\\bar\\baz.md', 'md'));
+    assert(!endsWith('foo\\bar\\baz.md', 'z.md'));
+  });
+
+  it('should be false when the path does not end with the string', function() {
+    assert(!endsWith('foo\\bar\\baz.md', '/'));
+    assert(!endsWith('foo\\bar\\baz.md', 'baz'));
+  });
+
+  it('should be false when the substring is a "partial" match', function() {
+    assert(!endsWith('foo\\bar\\bazqux', 'qux'));
+    assert(!endsWith('foo\\bar\\baz.md', 'baz'));
   });
 });
